d5ac892be54f46c9bf0eb5772df592cf330282c3
morcodou <morcodou@gmail.com>
2019-09-05 22:18:40 -0400

    new t

---
+++ command_line/text.cmd
@@ -0,0 +1,5 @@
+Justifying a background in computer science and over 10 years of experience in the fields of new information technology, I would like to apply for the position of INFORMATION SYSTEMS OFFICER.
+Indeed, my years of significant experience within the PCCI Group have allowed me to develop my ability to work as a team, an analytical mind but also to acquire a « savoir‐faire ».
+In addition, my stay at BU SSII, at Sigma-RH Solutions, Videotron, Qualicode Software, Semafo and Morneau Shepell has developed my ability to learn and open up to the new technologies, allowing me to evolve in the context of projects.
+It also has experience in the technologies of C, .NET, SQL and a good command of Windows environment, I am confident that I can bring a "new eye" to the developing your business.
+I hope that my knowledge will be a focus for you and I remain for more information
